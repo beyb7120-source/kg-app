9d68be3bdc98d32fd3be200469b1d5899f16570a
wdwedqwdkhlbvdgwedyieh <At412014@gmail.com>
2026-07-28 13:32:22 +0100

    sharing 1.6.1

--- js/app.js
+++ js/app.js
@@ -109,10 +109,7 @@ websitesBtn.addEventListener("click", () => {
     websitesModalBackdrop.classList.add("open");
 });
 
-const closeSubModals = () => {
-    copiedTextModalBackdrop.classList.remove("open");
-    websitesModalBackdrop.classList.remove("open");
-};
+
 closeCopiedTextModal.addEventListener("click", closeSubModals);
 closeWebsitesModal.addEventListener("click", closeSubModals);
 
